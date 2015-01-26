cbbba67671498b4459ced4498097757b8d1f4bb3
Brian Underwood <ml+github@semi-sentient.com>
2015-01-26 14:54:32 +0200

    Fix error in dummy thing

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "meta_model"
+require "neo4j/meta_model"
 
 module MetaModel
   class Application < Rails::Application
